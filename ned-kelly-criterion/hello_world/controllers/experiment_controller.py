7f3a41fb330ba97dd5640127e8261c1a39b20354
Leopold Tal G <leopold.tal.dg@gmail.com>
2021-10-03 12:17:45 +0200

    create all routes

--- ned-kelly-criterion/hello_world/controllers/experiment_controller.py
+++ ned-kelly-criterion/hello_world/controllers/experiment_controller.py
@@ -1,7 +1,33 @@
 import json
 from ..services.auth_service import get_current_user
-from ..services.experiment_service import create_experiment_with_variants, get_experiment_for_user
+from ..services.experiment_service import create_experiment_with_variants, create_variant, get_experiment_for_user
 from .formatters import format_collection, format_experiment, format_variant
+
+def get_experiment_suggest_variant(event, context):
+	user = get_current_user(event)
+	if user is None:
+		return { 'statusCode': 401 }
+	
+	# FIXME: how do I get the experiment id from the route?
+	experiment = get_experiment_for_user(1, user.user_id)
+	if experiment is None:
+		return { 'statusCode': 404 }
+	
+	if len(experiment.variants) == 0:
+		return {
+			'statusCode': 400,
+			'body': json.dumps({ 'message': 'Experiment has no variants' })
+		}
+	
+	# TODO: multi-armed bandit
+	chosen_variant = experiment.variants[0]
+	return {
+		'statusCode': 200,
+		'body': json.dumps({
+			'id': chosen_variant.id,
+			'name': chosen_variant.name
+		})
+	}
 
 def get_experiments(event, context):
 	user = get_current_user(event)
@@ -54,3 +80,16 @@ def post_experiment(event, context):
 	
 	# FIXME: how do I get postdata?
 	# create_experiment_with_variants(name = ?, user_id = user.user_id, variant_names = ?)
+
+def post_experiment_variant(event, context):
+	user = get_current_user(event)
+	if user is None:
+		return { 'statusCode': 401 }
+	
+	# FIXME: how do I get the experiment id from the route?
+	experiment = get_experiment_for_user(13, user.user_id)
+	if experiment is None:
+		return { 'statusCode': 404 }
+	
+	# FIXME: how do I get postdata?
+	# create_variant(name = ?)
